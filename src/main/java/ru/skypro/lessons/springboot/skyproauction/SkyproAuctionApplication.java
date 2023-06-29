c8fb695dad8f3973bfe677e40600dcefd697256e
klozz <ya.slava2323yaru@gmail.com>
2023-06-29 22:39:03 +0300

    Test

---
+++ src/main/java/ru/skypro/lessons/springboot/skyproauction/SkyproAuctionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.skyproauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproAuctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/skyproauction/SkyproAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.skyproauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
